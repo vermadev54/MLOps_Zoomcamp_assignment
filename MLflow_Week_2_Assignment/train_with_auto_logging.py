0b8b87f277e6e377da5d8745d60ffc1b21575bd7
jainendra <kumar>
2022-05-30 14:01:31 +0530

    week2 assignment added

---
+++ MLflow_Week_2_Assignment/train_with_auto_logging.py
@@ -0,0 +1,47 @@
+import argparse
+import os
+import pickle
+import mlflow
+
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, ExtraTreesRegressor
+from sklearn.svm import LinearSVR
+
+
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-homework_week_2")
+
+
+def load_pickle(filename: str):
+    with open(filename, "rb") as f_in:
+        return pickle.load(f_in)
+
+
+
+
+def run(data_path):
+    mlflow.sklearn.autolog()
+    with mlflow.start_run():
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--data_path",
+        default="./output",
+        help="the location where the processed NYC taxi trip data was saved."
+    )
+    args = parser.parse_args()
+
+    run(args.data_path)
